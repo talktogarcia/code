a7d6679d8d87035184d606b7d3d3af687c8241bb
Garcia Thomas <talktogarcia@gmail.com>
2021-08-26 15:00:15 +0530

    new Modification
    Dgt

---
+++ dgt/dgt_Api.js
@@ -0,0 +1,14 @@
+ 
+ body();
+ 
+  
+
+function body(){
+ 
+    $body = document.getElementById('dgt-setup').innerHTML = '<iframe src="https://garciathomas.in/" height="800px" width="100%" title="Loading DGT Services "></iframe> ';
+
+    
+}
+ 
+
+
